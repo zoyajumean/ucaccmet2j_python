f962c7a8f0e39a00e2ef2735bc274e0b3b2966f1
zoyajumean <zoyajimean05@gmail.com>
2025-01-14 16:49:45 +0100

    comments

--- precipitation.py
+++ precipitation.py
@@ -3,6 +3,7 @@ import csv
 #from collections import defaultdict
 #from datetime import datetime
 
+#the csv file that contains the state, location and station
 with open ('stations.csv') as file:
     stations= list(csv.DictReader (file))
 #print (stations)
@@ -15,11 +16,11 @@ with open ('precipitation.json',encoding='utf-8') as file:
 precipitation_info ={}
 total_precipitation_all_stations = 0
 
-
+#keep everything in the loop 
 for station in stations:
     #city_data
     station_number = station['Station']
-    state= station['State']
+    state= station['State']                         #CASE SENSITIVEEEEEE
     city_name= station['Location']
     city_data =[] #LISTTT
     for measurements in contents:
@@ -33,9 +34,9 @@ for station in stations:
     total_monthly_precipitation= {}
 
     for measurements in city_data:
-        month= (int(measurements['date'].split('-')[1]))
-        precipitation = measurements['value']
-        if month in total_monthly_precipitation:
+        month= (int(measurements['date'].split('-')[1]))   #list of strings that are now integers 
+        precipitation = measurements['value']              #that are split by the dash in the date
+        if month in total_monthly_precipitation:           #use the first index since python starts on 0
             total_monthly_precipitation[month] += precipitation
         else:
             total_monthly_precipitation [month]= 0
@@ -43,7 +44,7 @@ for station in stations:
     #print(total_monthly_precipitation)
 
 
-    #seattle yearly precipitation
+    #yearly precipitation for one station THEN ALL STATIONS
     total_yearly_precipitation= sum(total_monthly_precipitation.values())
     total_precipitation_all_stations += total_yearly_precipitation 
     #print(total_yearly_precipitation)
@@ -53,7 +54,7 @@ for station in stations:
     for month in total_monthly_precipitation:
         relative_monthly_precipitation[month] = (
             total_monthly_precipitation[month] / total_yearly_precipitation
-        )
+        )  #if total precipitation of all months is 100%...how much is one month????
 #print(relative_monthly_precipitation)
 
 
@@ -61,7 +62,7 @@ for station in stations:
     precipitation_info[city_name] = {
         'station': station_number,
         'state':state,
-        'total_monthly_precipitation': list(total_monthly_precipitation.values()),
+        'total_monthly_precipitation': list(total_monthly_precipitation.values()), #MAKE A LISSST 
         'total_yearly_precipitation': total_yearly_precipitation,
         'relative_monthly_precipitation': list(relative_monthly_precipitation.values())
         }  
@@ -69,8 +70,8 @@ for station in stations:
 for city_name, data in precipitation_info.items():
     data['relative_yearly_precipitation'] = (
         data['total_yearly_precipitation'] / total_precipitation_all_stations
-    )
-    
+    )   #this should be outside the loop 
+
 with open ('results.json', 'w') as file:
     json.dump(precipitation_info,file,indent=4)
 
